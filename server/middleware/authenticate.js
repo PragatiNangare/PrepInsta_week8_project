819be27aba0452f599d4fc19e7ededb117db8d32
Heetika Vaity <vaityheetika22@gmail.com>
2024-03-22 17:14:12 +0530

    made changes to authenticate.js

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,19 +1,20 @@
-// middleware/authenticate.js
 const jwt = require('jsonwebtoken');
 
 const authenticateUser = (req, res, next) => {
     const token = req.headers.authorization;
 
     if (!token) {
+        console.log('🦁 Token check failed: Authorization token is required');
         return res.status(401).json({ message: 'Authorization token is required' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log('🐘 Token decoded successfully:', decoded);
         req.user = decoded; // Attach user information to the request
         next();
     } catch (error) {
-        console.error('Authentication error:', error);
+        console.error('🐍 Authentication error:', error);
         return res.status(401).json({ message: 'Invalid token' });
     }
 };
